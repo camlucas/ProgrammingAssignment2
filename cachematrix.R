c9b2cdd758fd65012fc52e111cb3d9f32fd96572
Cam <cam.lucas.aus@gmail.com>
2014-12-17 21:55:54 +1100

    Comments added as well
    Comments added to file, assignment done.

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,18 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions, together, enable a matrix and it's inverse to be stored together
+## The matrix and it's inverse are stored inside the makeCacheMatrix environment,
+##   with the makeCachMatrix function only creating a list of four functions in the
+##   global environment
 
-## Write a short comment describing this function
+## To use these functions, first create/store the matrix inside makeCacheMatrix
+##      e.g. x <- makeCacheMatrix(MATRIX)
+## The invert/solve the matrix with cacheSolve()
+##      e.g. xinv <- cacheSolve(x)
+## If the matrix had been previously inverted, cacheSolve() will retrieve the solution
+##      - it will only revert to actually solving if no solution had been previously found
+
+
+
+## makeCacheMatrix creates the special "matrix", in reality a list of four functions
 
 makeCacheMatrix <- function(x = matrix()) {
   I <- NULL
@@ -18,7 +29,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## cacheSolve finds the inverse of a matrix created/stored with makeCacheMatrix
+## If previously solved, cacheSolve will recover and return the inverse from cache
+## Otherwise, it gets the original matrix, inverts it, 
+##    stores the inversion in the makeCacheMatrix environment, and returns the result
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
